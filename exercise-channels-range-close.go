82548f7c784b6bdd7f1b5d0cb74b7bcaacb2aef2
Juan Ortiz <juanfortizd@gmail.com>
2024-02-06 14:59:58 -0600

    add example for buffered channels

---
+++ exercise-channels-range-close.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fibonacci(index int, channel chan int) {
+	x, y := 0, 1
+	for i := 0; i < index; i++ {
+		channel <- x
+		x, y = y, x + y
+	}
+
+	close(channel)
+}
+
+func main() {
+	channel := make(chan int, 10)
+	go fibonacci(cap(channel), channel)
+
+	for value:= range channel {
+		fmt.Println(value)
+	}
+
+}
